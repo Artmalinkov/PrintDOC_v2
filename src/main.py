e75637789e8c6bc4f59afd1516408a0cee574d85
Artmalinkov <98594445+Artmalinkov@users.noreply.github.com>
2024-02-04 14:05:18 +0300

    Начало написания функционала

--- src/main.py
+++ src/main.py
@@ -3,4 +3,48 @@
 '''
 
 import openpyxl
+from docxtpl import DocxTemplate
 
+
+
+# Открытие и редактор шаблона Word
+def replace_Word_doc():
+    '''
+        Замена полей в шаблоне Word
+        :return: None
+        '''
+    # TODO проработать передачу пути
+    # TODO проработать передачу словаря значений
+    doc = DocxTemplate(r"attachments/word_tpl.docx")
+    context = {
+        'переменная': 'Название компании'
+       }
+    doc.render(context)
+    doc.save(r"done/generated_docx.docx")
+
+
+# формирование словаря где ключ - название поля таблицы, а значение - номер столбца
+import os
+from openpyxl import Workbook, load_workbook
+from openpyxl.worksheet.table import Table, TableStyleInfo
+
+wb = load_workbook(r'attachments/excel_tpl.xlsx')
+ws = wb.active
+table = ws.tables['Таблица1']
+
+
+table.ref
+
+for cell in ws[table.ref][0]:
+    print(cell.value)
+
+# TODO Получить индекс элемента в списке и продолжить тут
+for row in ws[table.ref]:
+    print(row)
+    for cell in row:
+        print (cell.value)
+    #print (row)
+
+
+def get_headers():
+    pass
