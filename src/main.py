0c16b27460f16261bff2379de59e9b0cac893afc
Artmalinkov <98594445+Artmalinkov@users.noreply.github.com>
2024-02-05 20:15:53 +0300

    Проработана функция получения словаря заголовков

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 '''
 Основной функционал приложения по автозамене данных в шаблонах Word
 '''
-
-import openpyxl
+import os
 from docxtpl import DocxTemplate
+from openpyxl import Workbook, load_workbook
+from openpyxl.worksheet.table import Table
 
+TEST_WORKBOOK_PATH = r'attachments/excel_tpl.xlsx'
+TEST_TABLE_NAME = 'Таблица1'
 
 
 # Открытие и редактор шаблона Word
@@ -18,35 +21,27 @@ def replace_Word_doc():
     doc = DocxTemplate(r"attachments/word_tpl.docx")
     context = {
         'переменная': 'Название компании'
-       }
+    }
     doc.render(context)
     doc.save(r"done/generated_docx.docx")
 
 
 # формирование словаря где ключ - название поля таблицы, а значение - номер столбца
-import os
-from openpyxl import Workbook, load_workbook
-from openpyxl.worksheet.table import Table, TableStyleInfo
+def get_headers(path_to_book: str,  table_name: str, ):
+    # Загрузка тестовой книги и получение таблицы внутри
+    wb = load_workbook(path_to_book)
+    ws = wb.active
+    table = ws.tables[table_name]
 
-# Загрузка тестовой книги и получение таблицы внутри
-wb = load_workbook(r'attachments/excel_tpl.xlsx')
-ws = wb.active
-table = ws.tables['Таблица1']
+    # Получение списка заголовков. Они находятся в первой строке таблицы
+    dict_headers = {}
+    for cell in ws[table.ref][0]:
+        dict_headers[cell.value] = cell.column
 
-# Получение списка заголовков. Они находятся в первой строке таблицы
-for cell in ws[table.ref][0]:
-    print(cell.column)
-
-test_cell = ws.cell(1,1)
-test_cell.row
-
-# TODO Получить индекс элемента в списке и продолжить тут
-for row in ws[table.ref]:
-    print(row)
-    for cell in row:
-        print (cell.value)
-    #print (row)
+    return dict_headers
 
 
-def get_headers():
-    pass
+
+
+test_dict_headers = get_headers(TEST_WORKBOOK_PATH, TEST_TABLE_NAME)
+test_dict_headers
---
+++ src/test.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+def get_headers():
+
+
+
+    pass
+
+
