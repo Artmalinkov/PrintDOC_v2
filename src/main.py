730a20129efc234a892c3fa7b4dcc36e7f09510e
Artmalinkov <98594445+Artmalinkov@users.noreply.github.com>
2024-02-05 21:32:20 +0300

    Проработана функция получения словаря нужной строки, где ключ - заголовок столбца, значение - значение ячейки в строке с индексом 'index_neces-row'

--- src/main.py
+++ src/main.py
@@ -33,29 +33,29 @@ def replace_Word_doc():
     doc.render(context)
     doc.save(r"done/generated_docx.docx")
 
-
-def get_headers(path_to_book: str, table_name: str):
-    '''
-    Формирование словаря где ключ - название поля таблицы, а значение - номер столбца
-    :param path_to_book: путь к рабочей книге excel
-    :param table_name: наименование таблицы
-    :return: dict_headers - словарь заголовков таблицы
-    '''
-    # Загрузка тестовой книги и получение таблицы внутри
-    wb = load_workbook(path_to_book)
-    ws = wb.active
-    table = ws.tables[table_name]
-
-    # Получение списка заголовков. Они находятся в первой строке таблицы
-    dict_headers = {}
-    for cell in ws[table.ref][0]:
-        dict_headers[cell.value] = cell.column
-
-    # TODO Проработать вынесение функционала загрузки рабочей униги отдельно
-    # TODO Прорпботать формирование этого словаря через атрибут column_names объекта table
-    # TODO Прорпботать формирование этого словаря через атрибут tableColumns объекта table
-
-    return dict_headers
+# Поскольку заголовки можно получать напрямую из атрибутов таблицы, словарь заголовков пока не нужен.
+# def get_headers(path_to_book: str, table_name: str):
+#     '''
+#     Формирование словаря где ключ - название поля таблицы, а значение - номер столбца
+#     :param path_to_book: путь к рабочей книге excel
+#     :param table_name: наименование таблицы
+#     :return: dict_headers - словарь заголовков таблицы
+#     '''
+#     # Загрузка тестовой книги и получение таблицы внутри
+#     wb = load_workbook(path_to_book)
+#     ws = wb.active
+#     table = ws.tables[table_name]
+#
+#     # Получение списка заголовков. Они находятся в первой строке таблицы
+#     dict_headers = {}
+#     for cell in ws[table.ref][0]:
+#         dict_headers[cell.value] = cell.column
+#
+#     # TODO Проработать вынесение функционала загрузки рабочей униги отдельно
+#     # TODO Прорпботать формирование этого словаря через атрибут column_names объекта table
+#     # TODO Прорпботать формирование этого словаря через атрибут tableColumns объекта table
+#
+#     return dict_headers
 
 
 def get_neces_row(worksheet, table, index_neces_row: int):
